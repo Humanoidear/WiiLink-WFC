4593601c35c9783d07271446d5e70b799dc96369
Alex <basurillodesgastorito@gmail.com>
2024-01-04 20:20:11 +0100

    Upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  server: {
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    }
+  },
+  adapter: vercel()
+});
