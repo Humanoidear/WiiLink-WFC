aa1f5a6cf02a28061d50d844c66ae613ab70b892
Alex <basurillodesgastorito@gmail.com>
2024-01-05 10:54:13 +0100

    Added test vercel CORS po

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  server: {
-    headers: {
-        "Access-Control-Allow-Origin": "*"
-    }
-  },
-  adapter: vercel()
-});
+export default defineConfig({});
