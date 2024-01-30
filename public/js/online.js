90d00ee4c9f5986ce6db1d8d2b7c73ecc29c4bb6
Alex <basurillodesgastorito@gmail.com>
2024-01-30 17:49:06 +0100

    Improved general layouts, optimized "online" page code and added "404" page

--- public/js/online.js
+++ public/js/online.js
@@ -24,8 +24,30 @@ var hash3 = "Unknown";
 var imgLang = "Unknown";
 var id = "Unknown";
 
+const coverUrls = [
+  "https://art.gametdb.com/wii/cover/US/",
+  "https://art.gametdb.com/wii/cover/EN/",
+  "https://art.gametdb.com/wii/cover/JA/",
+  "https://art.gametdb.com/wii/cover/KO/",
+  "/img/disc_placeholder.png",
+];
+
+const discUrls = [
+  "https://art.gametdb.com/wii/disc/US/",
+  "https://art.gametdb.com/wii/disc/EN/",
+  "https://art.gametdb.com/wii/disc/JA/",
+  "https://art.gametdb.com/wii/disc/KO/",
+  "/img/disc_placeholder.png",
+];
+
 var totalPlayers = 0;
 var totalGames = 0;
+
+function loadImage(element, urls, title) {
+  if (urls.length === 0) return;
+  element.src = urls[0] + title + ".png";
+  element.onerror = () => loadImage(element, urls.slice(1), title);
+}
 
 function render(xml, GameID) {
   var xmlDoc = xml.responseXML;
@@ -41,26 +63,21 @@ function render(xml, GameID) {
         // Loop through available locales
         var region = x[i].getElementsByTagName("region")[0].textContent;
         if (locales[j].getAttribute("lang") === desiredLang) {
-
           // Get all the data from the XML
           trueName = x[i].getAttribute("name") || "Unknown";
           developer =
-            x[i].getElementsByTagName("developer")[0]?.textContent ||
-            "Unknown";
+            x[i].getElementsByTagName("developer")[0]?.textContent || "Unknown";
           region =
             x[i].getElementsByTagName("region")[0]?.textContent || "Unknown";
           languages =
-            x[i].getElementsByTagName("languages")[0]?.textContent ||
-            "Unknown";
+            x[i].getElementsByTagName("languages")[0]?.textContent || "Unknown";
           publisher =
-            x[i].getElementsByTagName("publisher")[0]?.textContent ||
-            "Unknown";
+            x[i].getElementsByTagName("publisher")[0]?.textContent || "Unknown";
           date =
             x[i].getElementsByTagName("date")[0]?.getAttribute("year") ||
             "YYYY";
           month =
-            x[i].getElementsByTagName("date")[0]?.getAttribute("month") ||
-            "MM";
+            x[i].getElementsByTagName("date")[0]?.getAttribute("month") || "MM";
           day =
             x[i].getElementsByTagName("date")[0]?.getAttribute("day") || "DD";
           genre =
@@ -121,7 +138,31 @@ function render(xml, GameID) {
               break;
           }
 
-          var titleData = [trueName, developer, publisher, region, date, month, day, genre, rating, classification, wifiPlayers, isSupported, inputPlayers, languages, mainGenre, romName, romSize, romVersion, hash, hash2, hash3, imgLang, id];
+          var titleData = [
+            trueName,
+            developer,
+            publisher,
+            region,
+            date,
+            month,
+            day,
+            genre,
+            rating,
+            classification,
+            wifiPlayers,
+            isSupported,
+            inputPlayers,
+            languages,
+            mainGenre,
+            romName,
+            romSize,
+            romVersion,
+            hash,
+            hash2,
+            hash3,
+            imgLang,
+            id,
+          ];
           return titleData;
         }
       }
@@ -138,7 +179,7 @@ function displayTitleData(GameID) {
         var titleData = render(this, GameID);
         resolve(titleData);
       } else if (this.readyState == 4) {
-        reject('Error: ' + this.status);
+        reject("Error: " + this.status);
       }
     };
     xmlhttp.open("GET", "/xml/wiitdb.xml", true);
@@ -146,73 +187,111 @@ function displayTitleData(GameID) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch the list of all games
-  fetch("../json/gamespy_titles.json")
-    .then((response) => response.json())
-    .then(async (allGames) => {
-      // Fetch the list of online games
-      fetch("https://api.wfc.wiilink24.com/api/stats")
-        .then((response) => response.json())
-        .then(async (onlineStats) => {
-          // Loop through all games
-          for (let game of allGames) {
-            // Check if the game is in the list of online games
-            if (
-              onlineStats[game.GamespyName] &&
-              onlineStats[game.GamespyName].online > 0
-            ) {
-              // If the game is online, display it
-              var extraData = [onlineStats[game.GamespyName].online, onlineStats[game.GamespyName].active, onlineStats[game.GamespyName].groups];
-              var titleDataReturn = await displayTitleData(game.GameID);
-              // Create the HTML elements
-              var isBig = "";
-              if (extraData[1] > 6) {
-                isBig = "grid-column: auto / span 2; grid-row: auto / span 1;";
-              } else{
-                isBig = "grid-column: auto / span 1; grid-row: auto / span 1";
-              }
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    // Fetch the list of all games and the list of online games in parallel
+    const [allGamesResponse, onlineStatsResponse] = await Promise.all([
+      fetch("../json/gamespy_titles.json"),
+      fetch("https://api.wfc.wiilink24.com/api/stats"),
+    ]);
 
-              totalGames ++;
-              totalPlayers += extraData[0];
+    const allGames = await allGamesResponse.json();
+    const onlineStats = await onlineStatsResponse.json();
 
-              onlineNow.innerHTML +=
-                '<a href="/online/' + titleDataReturn[22] + '" class="card-online" style="' + isBig + '"><img src="https://art.gametdb.com/wii/cover/US/' +
-                titleDataReturn[22] +
-                '.png" onerror="this.onerror=null; this.src=\'https://art.gametdb.com/wii/cover/EN/' +
-                titleDataReturn[22] +
-                ".png'; this.onerror=function(){this.src='https://art.gametdb.com/wii/cover/JA/" +
-                titleDataReturn[22] +
-                ".png'; this.onerror=function(){this.src='https://art.gametdb.com/wii/cover/KO/" +
-                titleDataReturn[22] +
-                '.png\'; this.onerror=function(){this.src=\'/img/disc_placeholder.png\';};};};" id="card-bg" width="100%"><div style="display:flex; align-items:center; justify-content:space-between;"><img src="https://art.gametdb.com/wii/disc/US/' +
-                titleDataReturn[22] +
-                '.png" onerror="this.onerror=null; this.src=\'https://art.gametdb.com/wii/disc/EN/' +
-                titleDataReturn[22] +
-                ".png'; this.onerror=function(){this.src='https://art.gametdb.com/wii/disc/JA/" +
-                titleDataReturn[22] +
-                ".png'; this.onerror=function(){this.src='https://art.gametdb.com/wii/disc/KO/" +
-                titleDataReturn[22] +
-                '.png\'; this.onerror=function(){this.src=\'/img/disc_placeholder.png\';};};};" style="margin-right:20px;" width="70px">' +
-                '<div><h5 style="font-weight:800; text-align:right;">' +
-                titleDataReturn[0] +
-                "</h5><h6 style='text-align:right; opacity:0.5;'>" +
-                titleDataReturn[1] + " / " + titleDataReturn[2] + 
-                "</h6></div></div><p style='padding-top:20px; padding-bottom:25px; display:flex; gap:20px; align-items:center; justify-content:center;'><b style='text-align:center;'><b style='font-family:rubik; font-size:30px;'>" + extraData[0] + "</b><br><i class='fa-solid fa-user' style='margin-right:5px;'></i> online</b><b style='text-align:center;'><b style='font-family:rubik; font-size:30px;'>" + extraData[1] + "</b><br><i class='fa-solid fa-gamepad' style='margin-right:5px;'></i> Active</b><b style='text-align:center;'><b style='font-family:rubik; font-size:30px;'>" + extraData[2] + "</b><br><i class='fa-solid fa-users-rays' style='margin-right:5px;'></i>Groups</b></p>" +
-                titleDataReturn[7] + " <br><div style='display:flex; align-items:center; justify-content:space-between;'><p><span class='badge bg-translucent'>" +
-                titleDataReturn[3] +
-                "</span> <span class='badge bg-translucent'>" + titleDataReturn[4] +"</span></p><p><span class='badge bg-translucent'>" + titleDataReturn[9] + "-" + titleDataReturn[8] + "</span> <span class='badge bg-translucent'>" + titleDataReturn[12] + " <i class='fa-solid fa-user'></i> | " + titleDataReturn[10] + " <i class='fa-solid fa-earth-americas'></i></span></p></div></div></div></a>";
-            }
-            if(Object.keys(onlineStats).length < 2) {
-              document.getElementsByClassName("online-now-holder")[0].style.opacity = "0";
-              document.getElementsByClassName("online-now-holder")[0].style.pointerEvents = "none";
-              document.getElementById("noResults").style.display = "block";
-              document.getElementById("main").style.top = "45%";
-              document.getElementById("main").style.transform = "translate(-50%, -50%)";
-            }
-          }
-          document.getElementById("onlineInfo").innerHTML += "<p style='opacity:0.3;'>Now serving <b>" + totalPlayers + "</b> <i class='fa-solid fa-user' style='margin-right:5px;'></i> players in <b>" + totalGames + "</b> <i class='fa-solid fa-gamepad' style='margin-right:5px;'></i> games!</p>";
-        });
-    })
-    .catch((error) => console.error(error));
+    let totalGames = 0;
+    let totalPlayers = 0;
+
+    // Create an array of promises to process each game in parallel
+    const gamePromises = allGames.map(async (game) => {
+      if (onlineStats[game.GamespyName] && onlineStats[game.GamespyName].online > 0) {
+        const extraData = [
+          onlineStats[game.GamespyName].online,
+          onlineStats[game.GamespyName].active,
+          onlineStats[game.GamespyName].groups,
+        ];
+
+        const titleDataReturn = await displayTitleData(game.GameID);
+
+        // Create the image elements
+        const coverImgElement = document.createElement("img");
+        coverImgElement.id = "card-bg";
+        const discImgElement = document.createElement("img");
+        discImgElement.style.marginRight = "20px";
+        discImgElement.setAttribute("width", "70px");
+
+        // Use the loadImage function to load the images
+        loadImage(coverImgElement, coverUrls, titleDataReturn[22]);
+        loadImage(discImgElement, discUrls, titleDataReturn[22]);
+
+        var isBig = "";
+        if (extraData[1] > 6) {
+          isBig = "grid-column: auto / span 2; grid-row: auto / span 1;";
+        } else{
+          isBig = "grid-column: auto / span 1; grid-row: auto / span 1";
+        }
+
+        // Add the image elements to the HTML
+        onlineNow.innerHTML +=
+          '<a href="/online/' +
+          titleDataReturn[22] +
+          '" class="card-online" style="' +
+          isBig +
+          ' ">' +
+          coverImgElement.outerHTML +
+          '<div style="display:flex; align-items:center; justify-content:space-between;">' +
+          discImgElement.outerHTML +
+          '<div><h5 style="font-weight:800; text-align:right;">' +
+          titleDataReturn[0] +
+          "</h5><h6 style='text-align:right; opacity:0.5;'>" +
+          titleDataReturn[1] +
+          " / " +
+          titleDataReturn[2] +
+          "</h6></div></div><p style='padding-top:20px; padding-bottom:25px; display:flex; gap:20px; align-items:center; justify-content:center;'><b style='text-align:center;'><b style='font-family:rubik; font-size:30px;'>" +
+          extraData[0] +
+          "</b><br><i class='fa-solid fa-user' style='margin-right:5px;'></i> online</b><b style='text-align:center;'><b style='font-family:rubik; font-size:30px;'>" +
+          extraData[1] +
+          "</b><br><i class='fa-solid fa-gamepad' style='margin-right:5px;'></i> Active</b><b style='text-align:center;'><b style='font-family:rubik; font-size:30px;'>" +
+          extraData[2] +
+          "</b><br><i class='fa-solid fa-users-rays' style='margin-right:5px;'></i>Groups</b></p>" +
+          titleDataReturn[7] +
+          " <br><div style='display:flex; align-items:center; justify-content:space-between;'><p><span class='badge bg-translucent'>" +
+          titleDataReturn[3] +
+          "</span> <span class='badge bg-translucent'>" +
+          titleDataReturn[4] +
+          "</span></p><p><span class='badge bg-translucent'>" +
+          titleDataReturn[9] +
+          "-" +
+          titleDataReturn[8] +
+          "</span> <span class='badge bg-translucent'>" +
+          titleDataReturn[12] +
+          " <i class='fa-solid fa-user'></i> | " +
+          titleDataReturn[10] +
+          " <i class='fa-solid fa-earth-americas'></i></span></p></div></div></div></a>";
+
+        totalGames++;
+        totalPlayers += extraData[0];
+      }
+    });
+
+    // Wait for all game promises to resolve
+    await Promise.all(gamePromises);
+
+    if (Object.keys(onlineStats).length < 2) {
+      document.getElementsByClassName("online-now-holder")[0].style.opacity = "0";
+      document.getElementsByClassName("online-now-holder")[0].style.pointerEvents = "none";
+      document.getElementById("noResults").style.display = "block";
+      document.getElementById("main").style.top = "45%";
+      document.getElementById("main").style.transform = "translate(-50%, -50%)";
+    }
+
+    document.getElementById("onlineInfo").innerHTML +=
+      "<p style='opacity:0.3;'>Now serving <b>" +
+      totalPlayers +
+      "</b> <i class='fa-solid fa-user' style='margin-right:5px;'></i> players in <b>" +
+      totalGames +
+      "</b> <i class='fa-solid fa-gamepad' style='margin-right:5px;'></i> games!</p>";
+  } catch (error) {
+    console.error(error);
+  }
 });
+
